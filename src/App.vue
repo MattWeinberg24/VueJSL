8e10474267d9a252f65b9bb43cd2b8c5d9e54d1f
MattWeinberg24 <mattweinberg@wustl.edu>
2021-10-31 12:44:03 -0500

    enabled use of Kuroshiro

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,34 @@
 </template>
 
 <script>
-import wordJSON from './data/words.json'
-import VocabIO from './components/VocabIO.vue'
-import VocabOptions from './components/VocabOptions.vue'
+//import { loadScript } from "vue-plugin-load-script";
+import wordJSON from './data/words.json';
+import VocabIO from './components/VocabIO.vue';
+import VocabOptions from './components/VocabOptions.vue';
 
 export default {
   name: 'App',
   data() {
     return {
-      words: wordJSON
+      words: wordJSON,
+      kuroshiro: null
     }
   },
   mounted() {
     document.title = "JSLBot";
+
+    //set up external libraries
+      this.$loadScript('https://unpkg.com/kuroshiro@1.1.2/dist/kuroshiro.min.js').then(() => {
+        console.log("hello");
+        console.log(Kuroshiro.Util.isHiragana("a"));
+        console.log("what/?");
+      });
+    
+
+    
+  },
+  methods: {
+  
   },
   components: {
     VocabIO,
--- src/components/VocabIO.vue
+++ src/components/VocabIO.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="vocabio">
-        <div v-for="word in words.nominals" v-bind:key="word">{{word.japanese}}</div>
+        <div v-for="word in words.nominals" v-bind:key="word">{{word}}</div>
     </div>
 </template>
 
