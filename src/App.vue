6e0ac2cc92b10297660fcc84a40fb51d65889fd6
Matt Weinberg <mattw@pop-os.localdomain>
2021-11-03 22:09:23 -0500

    implemented kuroshiro translations

--- src/App.vue
+++ src/App.vue
@@ -14,39 +14,80 @@ export default {
   name: 'App',
   data() {
     return {
-      words: wordJSON,
+      wordData: wordJSON,
       kuroshiro: null
     }
   },
-  mounted() {
-    document.title = "JSLBot";
-
-    //set up external libraries
-    this.setupKuroshiro();
-    
-
-    
+  computed: {
+    words(){
+      var w = this.wordData;
+      return [].concat(w.nominals,w.verbals,w.na_nominals,w.adjectivals,w.suru_verbals, w.modifiers,w.greetings);
+    }
   },
   methods: {
-    setupKuroshiro(){
-      this.$loadScript('https://unpkg.com/kuroshiro-analyzer-kuromoji@1.1.0/dist/kuroshiro-analyzer-kuromoji.min.js').then(() => {
-        let analyzer = new KuromojiAnalyzer({ dictPath: "dict/" });
-        this.$loadScript('https://unpkg.com/kuroshiro@1.1.2/dist/kuroshiro.min.js').then(() => {
-          this.kuroshiro = new Kuroshiro();
-          this.kuroshiro.init(analyzer).then(() => {
-            console.log("Success!!!!!!!");
-          });
-        }).catch(e => {
-          console.log(e);
-        });
-      }).catch(e => {
-        console.log(e);
+  /**
+   * Loads external js libraries using vue-plugin-load-script
+   * @returns {Promise} Script loading processes
+   */
+    loadExternalScripts() {
+      var promiseArray = [];
+      promiseArray.push(this.$loadScript('https://unpkg.com/kuroshiro-analyzer-kuromoji@1.1.0/dist/kuroshiro-analyzer-kuromoji.min.js'));
+      promiseArray.push(this.$loadScript('https://unpkg.com/kuroshiro@1.1.2/dist/kuroshiro.min.js'));
+      return Promise.all(promiseArray);
+    },
+  /**
+   * Sets up the Kuroshiro library with the dictionary files. Used for kanji-kana translation
+   * @returns {Promise} Kuroshiro initialization process
+   */
+    initializeKuroshiro(){
+      let analyzer = new KuromojiAnalyzer({ dictPath: "dict/" });
+      this.kuroshiro = new Kuroshiro();
+      return this.kuroshiro.init(analyzer);
+    },
+  /**
+   * Generates kana and romaji translations for each word in the database
+   * @returns {Promise} The translation processes for every word
+   */
+    generateTranslations(){
+      var promiseArray = [];
+      this.words.forEach(word => {
+          if (word.hasOwnProperty("kana")){ //if kana is overridden in words.json
+              word.romazi = [];
+              word.kana.forEach(k => {
+                  word.romazi.push(this.nipponToJSL(Kuroshiro.Util.kanaToRomaji(k,"nippon")));
+              });
+          }
+          else { //if kana needs to be generated
+              word.kana = [];
+              word.romazi = [];
+              word.japanese.forEach((j,i) => {
+                  promiseArray.push(this.kuroshiro.convert(j, {to: "hiragana"}).then(k => {
+                      word.romazi.push(this.nipponToJSL(Kuroshiro.Util.kanaToRomaji(k,"nippon")));
+                      word.kana.push(k);
+                  }));
+              })
+          }
       });
+      
+      return Promise.all(promiseArray);
+    },
+
+   /**
+    * Converts Nippon romazi to JSL romazi
+    * @param {string} nippon Nippon romazi to convert
+    * @returns {string} The resulting JSL romazi
+    */
+    nipponToJSL(nippon) {
+        return nippon.replace("î","ii").replace("ô","oo").replace("ê","ee").replace("â","aa").replace("ou","oo").replace("ei","ee"); 
     }
   },
   components: {
     VocabIO,
     VocabOptions
+  },
+  mounted() {
+    document.title = "JSLBot";
+    this.loadExternalScripts().then(this.initializeKuroshiro).then(this.generateTranslations);
   }
 }
 </script>
--- src/components/VocabIO.vue
+++ src/components/VocabIO.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="vocabio">
-        <div v-for="word in words.nominals" v-bind:key="word">{{word}}</div>
+        <div v-for="word in words" v-bind:key="word">{{word}}</div>
     </div>
 </template>
 
